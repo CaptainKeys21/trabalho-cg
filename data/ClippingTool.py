ab7c9c63996e0609768f184115a88b472177ca1a
matias <matiasig2004@gmail.com>
2026-09-10 00:12:35 -0300

    Adicionada classe ClippingTool e seus métodos (vou consertar depois)

--- data/ClippingTool.py
+++ data/ClippingTool.py
@@ -13,75 +13,85 @@ class ClippingTool:
         return min(max(val, minVal), maxVal)
 
     def clipLine(self, point0: Tuple[float,float], point1: Tuple[float,float]):
-        code0 = self.region(point0)
-        code1 = self.region(point1)
-        if(code0 == 0 and code1 == 0): return point0, point1
-        if(code0 & code1 != 0): return (0,0), (0,0)
+        if(point0[0] < self.minX and point1[0] < self.minX) or (point0[1] < self.minY and point1[1] < self.minY) or (point0[0] > self.maxX and point1[0] > self.maxX) or (point0[1] > self.maxY and point1[1] > self.maxY):
+            return (0,0), (0, 0)
+        deltaX = point1[0] - point0[0]
+        deltaY = point1[1] - point0[1]
+        p = [-deltaX, deltaX, -deltaY, deltaY]
+        q = [point0[0] - self.minX, self.maxX - point0[0], point0[1] - self.minY, self.maxY - point0[1]]
+        ent = 0
+        ex = 1
 
-        slope = 0 if(point0[0] == point1[0] and point0[1] == point1[1]) else (point1[1] - point0[1])/(point1[0] - point0[0])
+        for i in range(4):
+            if p[i] == 0:
+                if q[1] < 0:
+                    return (0,0), (0,0)
+            else:
+                t = q[i]/p[i]
+                if p[i] < 0:
+                    if t > ent:
+                        ent = t
+                else:
+                    if t < ex:
+                        ex = t
 
-        slopeTL = (self.maxY - point0[1])/(self.minX - point0[0])
-        slopeTR = (self.maxY - point0[1])/(self.maxX - point0[0])
-        slopeBL = (self.minY - point0[1])/(self.minX - point0[0])
-        slopeBR = (self.minY - point0[1])/(self.maxX - point0[0])
+        if ent > ex: return (0,0), (0,0)
 
-        intersection = 0
-        if(slopeTL < slope and slope < slopeTR):
-            intersection |= 8
-        if(slopeTR < slope and slope < slopeBR):
-            intersection |= 2
-        if(slopeBR < slope and slope < slopeBL):
-            intersection |= 4
-        if(slopeBL < slope)
+        new0x = point0[0] + ent * deltaX
+        new0y = point0[1] + ent * deltaY
+        new1x = point0[0] + ex * deltaX
+        new1y = point0[1] + ex * deltaY
 
-        if(code0 == 0):
-            new0 = point0
-        else:
-            if(code0 & 8 != 0):
-                if()
+        return (new0x, new0y), (new1x, new1y)
+
+        new0 = point0
         new1 = point1
 
-        intersections = code0 & code1
+        p1 = -deltaX
+        p2 = deltaX
+        p3 = -deltaY
+        p4 = deltaY
 
-        print("\n", intersections, "\n")
+        q1 = point0[0] - self.minX
+        q2 = self.maxX - point0[0]
+        q3 = point0[1] - self.minY
+        q4 = self.maxY - point0[1]
 
-        if(intersections & 8 != 0):
-            topIntersection = (self.maxY - point1[1])/slope + point1[0]
-            if(code0 & 8): new0 = (topIntersection, self.maxY)
-            else: new1 = (topIntersection, self.maxY)
-        if(intersections & 4 != 0):
-            bottomIntersection = (self.minY - point1[1])/slope + point1[0]
-            if(code0 & 4): new0 = (bottomIntersection, self.minY)
-            else: new1 = (bottomIntersection, self.minY)
-        if(intersections & 2 != 0):
-            rightIntersection = (self.maxX - point1[0]) * slope + point1[1]
-            print(rightIntersection)
-            if(code0 & 8): new0 = (self.maxX, rightIntersection)
-            else: new1 = (self.maxX, rightIntersection)
-        if(intersections & 1 != 0):
-            leftIntersection = (self.minX - point1[0]) * slope + point1[1]
-            if(code0 & 8): new0 = (self.minX, leftIntersection)
-            else: new1 = (self.minX, leftIntersection)
+        if(deltaX == 0):
+            new0 = (point0[0], self.clipVal(point0[1], self.minY, self.maxY))
+            new1 = (point1[0], self.clipVal(point1[1], self.minY, self.maxY))
+        elif(deltaY == 0):
+            new0 = (self.clipVal(point0[0], self.minX, self.maxX), point0[1])
+            new1 = (self.clipVal(point1[0], self.minX, self.maxX), point1[1])
 
-        print("new0:", new0, "new1: ", new1)
-        return new0, new1 # 0,0;50,0;50,50;0,50  100,200;200,100;100,0;0,100
+        else:
+            zeta1 = max(0, q1/p1, q3/p3) if p1 < 0 else min(1, q1, q3)
+            zeta2 = min(1, q2/p2, q4/p3) if p2 < 0 else max(0, q2, q4)
+
+            print("zeta1: ", zeta1, ", zeta2: ", zeta2)
+
+            if(zeta1 > 0): new0 = point0[0] + zeta1 * deltaX, point0[1] + zeta1 * deltaY
+            if(zeta2 < 1): new1 = point1[0] + zeta2 * p2, point1[1] + zeta2 * p4
+
+        return new0, new1 # 0,0;50,0;50,50;0,50
 
     def clipPolygon(self, points: List[Tuple[float,float]]):
         newLines = []
         for i in range(0, len(points)):
-            if(i < len(points)- 1):
+            if(i < len(points) - 1):
                 n1, n2 = self.clipLine(points[i], points[i + 1])
             else:
                 n1, n2 = self.clipLine(points[i], points[0])
 
             if not (n1[0] == 0 and n1[1] == 0 and n2[0] == 0 and n2[1] == 0):
-                print("cand: ", n1, n2)
-                if(len(newLines) > 0): print(newLines[i - 1][0] != n1[0], newLines[i - 1][1] != n1[1])
-                if(len(newLines) == 0) or (newLines[i - 1][0] != n1[0] and newLines[i - 1][1] != n1[1]):
+                if not(self.contains(newLines, n1)):
                     newLines.append(n1)
-                    print("add ", n1)
-                newLines.append(n2)
-                print("add ", n2, "_")
+                    # print("add: ", n1)
+
+                if not (self.contains(newLines, n2)):
+                    # print(newLines)
+                    newLines.append(n2)
+                    # print("add: ", n1, "_")
 
         return newLines
 
@@ -94,3 +104,12 @@ class ClippingTool:
         right = 2 if point[0] > self.maxX else 0
         left = 1 if point[0] < self.minX else 0
         return top | bottom | right | left
+
+    def contains(self, points: List[Tuple[float, float]], point: Tuple[float, float]):
+        length = len(points)
+        for i in range(0, length):
+            if(points[i][0] == point[0] and points[i][1] == point[1]):
+                # print("true", points[i], point)
+                return True
+
+        return False
